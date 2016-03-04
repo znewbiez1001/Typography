54f0392a3eb64a19bc863c88c23a9f82dbe13f67
davishoang96 <davishoang96@gmail.com>
2016-03-05 01:25:58 +0700

    Add level to register page and moderator

--- function/add_moderator.php
+++ function/add_moderator.php
@@ -1,14 +1,4 @@
-<?php
-session_start();
-
-  if (isset($_SESSION['USERNAME'])) {
-    echo "welcome, ".$_SESSION['USERNAME']. "!";
-  } else {
-    die('You must be logged in');
-  }
-
-
-?>
+<!doctype html>
 <html>
 <?php include '../core/head.php'; ?>
 
@@ -39,12 +29,17 @@ session_start();
         if (isset($_POST['ADD'])) {
             include 'connecttk.php';
 
+            $name = mysql_escape_string($_POST['NAME']);
             $username = mysql_escape_string($_POST['USERNAME']);
-            $password = SHA1($_POST['PASSWORD']);
-            $vpassword = SHA1($_POST['VPASSWORD']);
+            $email = $_POST['EMAIL'];
+            $vemail = $_POST['VEMAIL'];
+            $pass = SHA1($_POST['PASSWORD']); //SHA1: PASSWORD ENCRYPTION
+            $vpass = SHA1($_POST['VPASSWORD']);
+            $add = $_POST['ADDRESS'];
+            $dob = $_POST['DOB'];
             $level = $_POST['LEVEL'];
 
-            if (empty($_POST['USERNAME']) || empty($_POST['PASSWORD']) || empty($_POST['VPASSWORD']) || empty($_POST['LEVEL'])) {
+            if (empty($_POST['NAME']) || empty($_POST['USERNAME']) || empty($_POST['EMAIL']) || empty($_POST['PASSWORD']) || empty($_POST['VPASSWORD']) || empty($_POST['ADDRESS']) || empty($_POST['DOB']) || empty($_POST['LEVEL'])) {
                 ?>
 								<script type="text/javascript">
 								alert("DON'T LEAVE ANY BLANK IN THE FORM");
@@ -54,7 +49,7 @@ session_start();
 
             }
 
-            $result = mysql_query("SELECT * FROM moderator WHERE USERNAME ='$username'", $conn);
+            $result = mysql_query("SELECT * FROM register WHERE USERNAME ='$username'", $conn);
             if (mysql_num_rows($result) > 0) {
                 while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
                     if ($row['USERNAME'] == $username) {
@@ -66,7 +61,14 @@ session_start();
 
                     }
                 }
-            } elseif ($password != $vpassword) {
+            } elseif ($email != $vemail) {
+                ?>
+								<script type="text/javascript">
+									alert("EMAIL DID NOT MATCH");
+								</script>
+								<?php
+
+            } elseif ($pass != $vpass) {
                 ?>
 								<script type="text/javascript">
 									alert("PASSWORD DID NOT MATCH!! RE-ENTER");
@@ -74,7 +76,7 @@ session_start();
 								<?php
 
             } else {
-                $str = "insert into moderator values ('','$username','$password','$level')";
+                $str = "insert into register values ('','$name','$username','$email','$pass','$add','$dob','$level')";
                 mysql_query($str, $conn);
                 ?>
 								"<SCRIPT LANGUAGE='JavaScript'>
@@ -106,7 +108,12 @@ session_start();
 					<div class="panel-body">
 						<form class="form-horizontal" role="form" method="post" onsubmit="return checkform()" name="reg">
 
-
+							<div class="form-group">
+								<label>
+									Name
+								</label>
+								<input name="NAME"  class="form-control"  />
+							</div>
 
 							<div class="form-group">
 								<label>
@@ -115,7 +122,19 @@ session_start();
 								<input name="USERNAME"  class="form-control"  />
 							</div>
 
+							<div class="form-group">
+								<label>
+									Email
+								</label>
+								<input name="EMAIL"  class="form-control" type="email"  />
+							</div>
 
+							<div class="form-group">
+								<label>
+									Verify Email
+								</label>
+								<input name="VEMAIL"  class="form-control" type="email"  />
+							</div>
 
 							<div class="form-group">
 								<label>
@@ -131,15 +150,25 @@ session_start();
 								<input name="VPASSWORD"  class="form-control" type="password"  />
 							</div>
 
-              <div class="form-group">
-								<label class="checkbox-inline"><input type="checkbox" value="1" name="LEVEL">Admin</label>
-                <label class="checkbox-inline"><input type="checkbox" value="0" name="LEVEL">User</label>
+
+						<div class="form-group">
+								<label>
+									Address
+								</label>
+								<input name="ADDRESS"  class="form-control" type="text"  />
 							</div>
 
+							<div class="form-group">
+								<label>
+									Date of birth
+								</label>
+								<input name="DOB"  class="form-control" type="date"  />
+							</div>
 
-
-
-
+              <div class="form-group">
+                <label class="checkbox-inline"><input type="checkbox" value="1" name="LEVEL">Admin</label>
+                <label class="checkbox-inline"><input type="checkbox" value="0" name="LEVEL">User</label>
+              </div>
 
 							<div class="text-center">
 								<button class="btn btn-success" type="submit" name="ADD">
